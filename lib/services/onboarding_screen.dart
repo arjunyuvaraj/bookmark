26072ec0328d42e93b82e5be888dae7ca40a504d
arjunyuvaraj <arjyuvaraj@gmail.com>
2026-01-09 12:10:00 -0500

    making aayush happy

---
+++ lib/services/onboarding_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OnbardingScreen extends StatelessWidget {
+  const OnbardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
